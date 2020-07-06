bcb466338af0b3445caa3d781a809f7180c90489
Kristiyan Ivanov <kristiqn92@gmail.com>
2020-07-06 16:43:06 +0300

    Added slug to the Category model. And set the slug field to be prepopulated by the admin.

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
 # Register your models here.
-admin.site.register(Category)
 
 
+# Class to modify the Category interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+# Class to modify the Page interface
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
+# Update the model admin registrations
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
