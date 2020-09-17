94c8703f69f3ccdac643b0cba8264438b6f4de1a
Kristiyan Ivanov <kristiqn92@gmail.com>
2020-09-17 16:16:50 +0300

    Finished chapter 11 - Implementation of Registration-Redux apo

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from rango.forms import CategoryForm, PageForm
 from datetime import datetime
 
 
@@ -146,72 +144,6 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context=context_dict)
 
 
-def register(request):
-    # boolean value that tells the template whether the
-    # registration was successful or not
-    registered = False
-
-    if request.method == 'POST':
-        user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            # saves the user to the database
-            user = user_form.save()
-
-            # hash and set password
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # checks if the user provided a picture
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            profile.save()
-            registered = True
-        else:
-            # Invalit form
-            print(user_form.errors, profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-
-    return render(request, 'rango/register.html', context={'user_form': user_form,
-                                                           'profile_form': profile_form,
-                                                           'registered': registered})
-
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                login(request, user)
-                return redirect(reverse('rango:index'))
-            else:
-                return HttpResponse("Your Rango account is disabled.")
-
-        else:
-            print(f"Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid login details supplied.")
-
-    else:
-        return render(request, 'rango/login.html')
-
-
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
-
-
-@login_required
-def user_logout(request):
-    logout(request)
-    return redirect(reverse('rango:index'))
